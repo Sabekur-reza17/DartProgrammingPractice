6fbb6b43bd339f81758f895e5d224f905784b869
sabekur <sabekur2020@gmail.com>
2020-01-07 10:58:14 +0600

    started dart programming

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:DartProgrammingPractice/DartProgrammingPractice.dart' as DartProgrammingPractice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${DartProgrammingPractice.calculate()}!');
+}
---
+++ test/DartProgrammingPractice_test.dart
@@ -0,0 +1,8 @@
+import 'package:DartProgrammingPractice/DartProgrammingPractice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
